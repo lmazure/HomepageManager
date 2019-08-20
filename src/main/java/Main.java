12ea196207c703c19e43c728ecad5ab028114910
Laurent <Laurent@gilgamesh>
2019-08-20 13:43:49 +0200

    Added parsing of the command line.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,27 @@
+import java.io.File;
+
 public class Main {
 
-    public static void main(String[] args) {
+    final static private String s_markerFile = "google1b78f05130a6dbb0.html";
+
+    public static void main(final String[] args) {
+        
+        if (args.length != 1) {
+            System.err.println("Syntax: HomepageManager <homepage directory>");
+            System.exit(1);             
+        }
+ 
+        final Main main = new Main();
+        main.start(args[0]);
+    }
+
+    private void start(final String homepagePath) {
+
+        if (!(new File(homepagePath + File.separator + s_markerFile)).exists()) {
+            System.err.println(homepagePath + " does not contain the homepage");
+            System.exit(1);             
+        }
+
         System.out.println("Done!");
     }
 
