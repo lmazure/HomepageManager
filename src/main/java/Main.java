5faab92c4ad7114b0a31140e1ec2a966b7e145ec
Laurent <Laurent@gilgamesh>
2019-10-13 07:59:30 +0200

    UI implementation: Fixed the layout.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,6 +9,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
@@ -25,12 +26,11 @@ public class Main extends Application {
     static private Path _homepagePath;
     static private Path _tmpPath;
  
-    private final TableView<TrackedFile> _table;
+    private TableView<TrackedFile> _table;
     private final ObservableList<TrackedFile> _data;
     
     public Main() {
         _data = FXCollections.observableArrayList();
-        _table = new TableView<TrackedFile>();
     }
     
     public static void main(final String[] args) {
@@ -47,36 +47,25 @@ public class Main extends Application {
  
     @Override
     public void start(Stage stage) {
-        final Scene scene = new Scene(new Group());
         stage.setTitle("Homepage Manager");
         stage.setWidth(900);
         stage.setHeight(500);
  
-        final Label label = new Label("Files");
-        label.setFont(new Font("Arial", 20));
- 
+        _table = new TableView<TrackedFile>();
         final TableColumn<TrackedFile, String> fileColumn = new TableColumn<TrackedFile, String>("File");
         fileColumn.setCellValueFactory(cellData -> cellData.getValue().nameProperty());
-
         final TableColumn<TrackedFile, Boolean> deletedColumn = new TableColumn<TrackedFile, Boolean>("Deleted");
         deletedColumn.setCellValueFactory(cellData -> cellData.getValue().deletedProperty());
-
         final TableColumn<TrackedFile, String> htmlStatusColumn = new TableColumn<TrackedFile, String>("HTML");
         htmlStatusColumn.setCellValueFactory(cellData -> cellData.getValue().htmlFileProperty());
-
         final TableColumn<TrackedFile, String> fileCheckColumn = new TableColumn<TrackedFile, String>("Check");
         fileCheckColumn.setCellValueFactory(cellData -> cellData.getValue().fileCheckProperty());
-
         _table.getColumns().addAll(fileColumn, deletedColumn, htmlStatusColumn, fileCheckColumn);
         _table.setItems(_data);
  
-        final VBox vbox = new VBox();
-        vbox.setSpacing(5);
-        vbox.setPadding(new Insets(10, 0, 0, 10));
-        vbox.getChildren().addAll(label, _table);
- 
-        ((Group)scene.getRoot()).getChildren().addAll(vbox);
- 
+        final BorderPane border = new BorderPane();
+        border.setCenter(_table);
+        final Scene scene = new Scene(border);
         
         final Service<Void> calculateService = new Service<Void>() {
             @Override
