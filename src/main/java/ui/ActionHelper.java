b8a5c1632ce39e059e8afa31f68cc08343642376
Laurent MAZURE <lma@infologic.fr>
2021-04-26 17:56:26 +0200

    Fixed crash when trying to display a file that has just been removed.

--- src/main/java/ui/ActionHelper.java
+++ src/main/java/ui/ActionHelper.java
@@ -2,6 +2,8 @@ package ui;
 
 import java.awt.Desktop;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.net.HttpURLConnection;
 import java.net.URISyntaxException;
 import java.net.URL;
@@ -11,6 +13,10 @@ import javax.net.ssl.SSLHandshakeException;
 
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
 import utils.ExitHelper;
 import utils.StringHelper;
 
@@ -72,11 +78,14 @@ public class ActionHelper {
         try {
             Desktop.getDesktop().browse(file.toUri());
         } catch (final IOException e) {
-            ExitHelper.exit(e);
+            displayExceptionError("Error",
+                                  "File display error",
+                                  "An exception occurred while displaying file " + file.toString(),
+                                  e);
         }
     }
 
-    static boolean isUrlAlive(final URL url) {
+    private static boolean isUrlAlive(final URL url) {
         try {
             final HttpURLConnection huc = (HttpURLConnection)url.openConnection();
             final int responseCode = huc.getResponseCode();
@@ -84,5 +93,36 @@ public class ActionHelper {
         } catch (final IOException e) {
             return e instanceof SSLHandshakeException;
         }
+    }
+
+    private static void displayExceptionError(final String dialogTitle,
+                                              final String header,
+                                              final String errorMessage,
+                                              final Throwable exception) {
+        // from https://code.makery.ch/blog/javafx-dialogs-official/
+        final Alert alert = new Alert(AlertType.ERROR);
+        alert.setTitle(dialogTitle);
+        alert.setHeaderText(header);
+        alert.setContentText(errorMessage);
+
+        final StringWriter sw = new StringWriter();
+        final PrintWriter pw = new PrintWriter(sw);
+        exception.printStackTrace(pw);
+        final String exceptionText = sw.toString();
+        Label label = new Label("The exception stacktrace was:");
+        final TextArea textArea = new TextArea(exceptionText);
+
+        textArea.setEditable(false);
+        textArea.setWrapText(true);
+        textArea.setMaxWidth(Double.MAX_VALUE);
+        textArea.setMaxHeight(Double.MAX_VALUE);
+        GridPane.setVgrow(textArea, Priority.ALWAYS);
+        GridPane.setHgrow(textArea, Priority.ALWAYS);
+        GridPane expContent = new GridPane();
+        expContent.setMaxWidth(Double.MAX_VALUE);
+        expContent.add(label, 0, 0);
+        expContent.add(textArea, 0, 1);
+        alert.getDialogPane().setExpandableContent(expContent);
+        alert.showAndWait();
     }
 }
