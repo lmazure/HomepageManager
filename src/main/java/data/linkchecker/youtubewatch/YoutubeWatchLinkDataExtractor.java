ff537b4303978c2e527b92822a126880a2a4fbc8
Laurent <Laurent@gilgamesh>
2022-02-22 22:29:07 +0100

    First stab at supporting probable authors

--- src/main/java/data/linkchecker/youtubewatch/YoutubeWatchLinkDataExtractor.java
+++ src/main/java/data/linkchecker/youtubewatch/YoutubeWatchLinkDataExtractor.java
@@ -10,6 +10,8 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import data.linkchecker.ContentParserException;
 import data.linkchecker.ExtractedLinkData;
@@ -21,81 +23,111 @@ public class YoutubeWatchLinkDataExtractor extends data.linkchecker.LinkDataExtr
     private final static Map<String, ChannelData> _channelData = Map.ofEntries(
             new AbstractMap.SimpleEntry<>("3Blue1Brown",
                                           new ChannelData(buildList(buildAuthor("Grant", "Sanderson")),
+                                                          buildMatchingList(),
                                                           Locale.ENGLISH)),
             new AbstractMap.SimpleEntry<>("AstronoGeek",
                                           new ChannelData(buildList(buildAuthor("Arnaud", "Thiry")),
+                                                          buildMatchingList(),
                                                           Locale.FRENCH)),
             new AbstractMap.SimpleEntry<>("Balade Mentale",
                                           new ChannelData(buildList(buildAuthor("Théo", "Drieu")),
+                                                          buildMatchingList(),
                                                           Locale.FRENCH)),
             new AbstractMap.SimpleEntry<>("Chat Sceptique",
                                           new ChannelData(buildList(buildAuthor("Nathan", "Uyttendaele")),
+                                                          buildMatchingList(),
                                                           Locale.FRENCH)),
             new AbstractMap.SimpleEntry<>("Dr. Becky",
                                           new ChannelData(buildList(buildAuthor("Becky", "Smethurst")),
+                                                          buildMatchingList(),
+                                                          Locale.ENGLISH)),
+            new AbstractMap.SimpleEntry<>("DeepSkyVideos",
+                                          new ChannelData(buildList(),
+                                                          buildMatchingList(match("Crowther", buildAuthor("Paul", "Crowther"))),
                                                           Locale.ENGLISH)),
             new AbstractMap.SimpleEntry<>("Heu?reka",
                                           new ChannelData(buildList(buildAuthor("Gilles", "Mitteau")),
+                                                          buildMatchingList(),
                                                           Locale.FRENCH)),
             new AbstractMap.SimpleEntry<>("J'm'énerve pas, j'explique",
                                           new ChannelData(buildList(buildAuthor("Bertrand", "Augustin")),
+                                                          buildMatchingList(),
                                                           Locale.FRENCH)),
             new AbstractMap.SimpleEntry<>("History of the Earth",
                                           new ChannelData(buildList(buildAuthor("David", "Kelly"), buildAuthor("Pete", "Kelly"), buildAuthor("Kelly", "Battison")),
+                                                          buildMatchingList(),
                                                           Locale.ENGLISH)),
             new AbstractMap.SimpleEntry<>("Le Réveilleur",
                                           new ChannelData(buildList(buildAuthor("Rodolphe", "Meyer")),
+                                                          buildMatchingList(),
                                                           Locale.FRENCH)),
             new AbstractMap.SimpleEntry<>("Mathologer",
                                           new ChannelData(buildList(buildAuthor("Burkard", "Polster")),
+                                                          buildMatchingList(),
                                                           Locale.ENGLISH)),
             new AbstractMap.SimpleEntry<>("monsieur bidouille",
                                           new ChannelData(buildList(buildAuthor("Dimitri", "Ferrière")),
+                                                          buildMatchingList(),
                                                           Locale.FRENCH)),
             new AbstractMap.SimpleEntry<>("Monsieur Phi",
                                           new ChannelData(buildList(buildAuthor("Thibaut", "Giraud")),
+                                                          buildMatchingList(),
                                                           Locale.FRENCH)),
             new AbstractMap.SimpleEntry<>("Passe-Science",
                                           new ChannelData(buildList(buildAuthor("Thomas", "Cabaret")),
+                                                          buildMatchingList(),
                                                           Locale.FRENCH)),
             new AbstractMap.SimpleEntry<>("Quadriviuum Tremens",
                                           new ChannelData(buildList(buildAuthor("Tristan", "Audam-Dabidin"), buildAuthor("Keshika", "Dabidin")),
+                                                          buildMatchingList(),
                                                           Locale.FRENCH)),
             new AbstractMap.SimpleEntry<>("Robert Miles",
                                           new ChannelData(buildList(buildAuthor("Robert", "Miles")),
+                                                          buildMatchingList(),
                                                           Locale.ENGLISH)),
             new AbstractMap.SimpleEntry<>("Sabine Hossenfelder",
                                           new ChannelData(buildList(buildAuthor("Sabine", "Hossenfelder")),
+                                                          buildMatchingList(),
                                                           Locale.ENGLISH)),
             new AbstractMap.SimpleEntry<>("ScienceClic",
                                           new ChannelData(buildList(buildAuthor("Alessandro", "Roussel")),
+                                                          buildMatchingList(),
                                                           Locale.FRENCH)),
             new AbstractMap.SimpleEntry<>("ScienceEtonnante",
                                           new ChannelData(buildList(buildAuthor("David", "Louapre")),
+                                                          buildMatchingList(),
                                                           Locale.FRENCH)),
             new AbstractMap.SimpleEntry<>("Stand-up Maths",
                                           new ChannelData(buildList(buildAuthor("Matt", "Parker")),
+                                                          buildMatchingList(),
                                                           Locale.ENGLISH)),
             new AbstractMap.SimpleEntry<>("Science4All",
                                           new ChannelData(buildList(buildAuthor("Lê", "Nguyên Hoang")),
+                                                          buildMatchingList(),
                                                           Locale.FRENCH)),
             new AbstractMap.SimpleEntry<>("Thomaths",
                                           new ChannelData(buildList(buildAuthor("Alexander", "Thomas")),
+                                                          buildMatchingList(),
                                                           Locale.FRENCH)),
             new AbstractMap.SimpleEntry<>("Tom Scott",
                                           new ChannelData(buildList(buildAuthor("Tom", "Scott")),
+                                                          buildMatchingList(),
                                                           Locale.ENGLISH)),
             new AbstractMap.SimpleEntry<>("Tric Trac",
                                           new ChannelData(buildList(buildAuthor("Guillaume", "Chifoumi"), buildAuthor("François", "Décamp")),
+                                                          buildMatchingList(),
                                                           Locale.FRENCH)),
             new AbstractMap.SimpleEntry<>("Veritasium",
                                           new ChannelData(buildList(buildAuthor("Derek", "Muller")),
+                                                          buildMatchingList(),
                                                           Locale.ENGLISH)),
             new AbstractMap.SimpleEntry<>("Web Dev Simplified",
                                           new ChannelData(buildList(buildAuthor("Kyle", "Cook")),
+                                                          buildMatchingList(),
                                                           Locale.ENGLISH)),
             new AbstractMap.SimpleEntry<>("WonderWhy",
                                           new ChannelData(buildList(buildAuthorFromGivenName("WonderWhy")),
+                                                          buildMatchingList(),
                                                           Locale.ENGLISH))
             );
     private final YoutubeWatchLinkContentParser _parser;
@@ -130,13 +162,33 @@ public class YoutubeWatchLinkDataExtractor extends data.linkchecker.LinkDataExtr
     }
 
     @Override
-    public List<AuthorData> getProbableAuthors() {
-        return new ArrayList<>(0);
+    public List<AuthorData> getProbableAuthors() throws ContentParserException {
+        final List<AuthorData> authors = new ArrayList<>();
+        final String channel = _parser.getChannel();
+        if (_channelData.containsKey(channel)) {
+            for (MatchingAuthor match: _channelData.get(channel).getMatchingAuthors()) {
+                final Matcher m = match.getPattern().matcher(_parser.getDescription());
+                if (m.find()) {
+                   authors.add(match.getAuthor());
+                 }
+            }
+        }
+        return authors;
     }
 
     @Override
-    public List<AuthorData> getPossibleAuthors()  {
-        return new ArrayList<>(0);
+    public List<AuthorData> getPossibleAuthors() throws ContentParserException  {
+        final List<AuthorData> authors = new ArrayList<>();
+        final String channel = _parser.getChannel();
+        if (_channelData.containsKey(channel)) {
+            for (MatchingAuthor match: _channelData.get(channel).getMatchingAuthors()) {
+                final Matcher m = match.getPattern().matcher(_parser.getDescription());
+                if (!m.find()) {
+                   authors.add(match.getAuthor());
+                 }
+            }
+        }
+        return authors;
     }
 
     @Override
@@ -162,6 +214,10 @@ public class YoutubeWatchLinkDataExtractor extends data.linkchecker.LinkDataExtr
         return Arrays.asList(authors);
     }
 
+    private static List<MatchingAuthor> buildMatchingList(final MatchingAuthor ...authors) {
+        return Arrays.asList(authors);
+    }
+
     private static AuthorData buildAuthor(final String firstName,
                                           final String lastName) {
     return new AuthorData(Optional.empty(),
@@ -179,24 +235,55 @@ public class YoutubeWatchLinkDataExtractor extends data.linkchecker.LinkDataExtr
                               Optional.empty(),
                               Optional.empty(),
                               Optional.of(givenName));
-}
-
-   private static class ChannelData {
-       final List<AuthorData> _authors;
-       final Locale _language;
-
-       public ChannelData(final List<AuthorData> authors,
-                          final Locale language) {
-        _authors = authors;
-        _language = language;
     }
 
-    private List<AuthorData> getAuthors() {
-        return _authors;
+    private static MatchingAuthor match(final String regexp,
+                                        final AuthorData author) {
+        return new MatchingAuthor(Pattern.compile(regexp), author);
     }
+    
+    private static class ChannelData {
+        private final List<AuthorData> _authors;
+        private final List<MatchingAuthor> _matchingAuthors;
+        private final Locale _language;
 
-    private Locale getLanguage() {
-        return _language;
-    }
+        public ChannelData(final List<AuthorData> authors,
+                           final List<MatchingAuthor> matchingAuthors,
+                           final Locale language) {
+            _authors = authors;
+            _matchingAuthors = matchingAuthors;
+            _language = language;
+        }
+
+        private List<AuthorData> getAuthors() {
+            return _authors;
+        }
+
+        private List<MatchingAuthor> getMatchingAuthors() {
+            return _matchingAuthors;
+        }
+
+        private Locale getLanguage() {
+            return _language;
+        }
+   }
+   
+    private static class MatchingAuthor {
+        private final Pattern _pattern;
+        private final AuthorData _author;
+
+        public MatchingAuthor(final Pattern pattern,
+                              final AuthorData author) {
+            _pattern = pattern;
+            _author = author;
+        }
+
+        private Pattern getPattern() {
+            return _pattern;
+        }
+
+        private AuthorData getAuthor() {
+            return _author;
+        }
    }
 }
