084b292134be6d58d8582c574cba81790405d7fa
Laurent <laurent@gilgamesh>
2022-11-17 07:59:47 +0100

    Rewrote start of Simple Web Server

--- src/main/java/utils/internet/WebServer.java
+++ src/main/java/utils/internet/WebServer.java
@@ -1,20 +1,38 @@
 package utils.internet;
 
+import java.io.IOException;
 import java.net.InetSocketAddress;
+
+import com.sun.net.httpserver.Filter;
+import com.sun.net.httpserver.HttpHandler;
 import java.nio.file.Path;
 
 import com.sun.net.httpserver.HttpServer;
 import com.sun.net.httpserver.SimpleFileServer;
 
+import utils.ExitHelper;
 import utils.Logger;
 
+/**
+ * Management of the local Web Server
+ *
+ */
 public class WebServer {
 
     public static void start(final Path homepagePath) {
-        final HttpServer server = SimpleFileServer.createFileServer(new InetSocketAddress(80),
-                                                                    homepagePath,
-                                                                    SimpleFileServer.OutputLevel.VERBOSE);
-        server.start();
+        final InetSocketAddress address = new InetSocketAddress(80);
+        final HttpHandler handler = SimpleFileServer.createFileHandler(homepagePath);
+        final Filter filter = SimpleFileServer.createOutputFilter(System.out, SimpleFileServer.OutputLevel.VERBOSE);
+        try {
+            final HttpServer server = HttpServer.create(address,
+                                                        10,
+                                                        "/",
+                                                        handler,
+                                                        filter);
+            server.start();
+        } catch (final IOException e) {
+            ExitHelper.exit("Failed to create Web Server", e);
+        }
         Logger.log(Logger.Level.INFO)
               .append("server is started")
               .submit();
