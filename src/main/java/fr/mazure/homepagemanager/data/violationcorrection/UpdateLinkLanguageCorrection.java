3b4d3a252083d07ceed748ddf65d0e886d21e7ce
Laurent <Laurent@gilgamesh.home>
2023-03-04 12:53:03 +0100

    Added UpdateLinkLanguageCorrection

---
+++ src/main/java/fr/mazure/homepagemanager/data/violationcorrection/UpdateLinkLanguageCorrection.java
@@ -0,0 +1,24 @@
+package fr.mazure.homepagemanager.data.violationcorrection;
+
+import java.util.Locale;
+import java.util.regex.Pattern;
+
+/**
+ * Correct the language of a link
+ */
+public class UpdateLinkLanguageCorrection extends RegexpViolationCorrection {
+
+    /**
+     * Constructor
+     * @param badLanguage Incorrect language
+     * @param correctLanguage  Correct language
+     * @param url URL of the link
+     */
+    public UpdateLinkLanguageCorrection(final Locale badLanguage,
+                                        final Locale correctLanguage,
+                                        final String url) {
+        super("Update the article title",
+              "<A>" + Pattern.quote(url) + "</A><L>" + badLanguage.getLanguage() + "</L>",
+              "<A>" + url + "</A><L>" + correctLanguage.getLanguage() + "</L>");
+    }
+}
---
+++ src/test/java/fr/mazure/homepagemanager/data/violationcorrection/test/UpdateLinkLanguageCorrectionTest.java
@@ -0,0 +1,55 @@
+package fr.mazure.homepagemanager.data.violationcorrection.test;
+
+import java.util.Locale;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import fr.mazure.homepagemanager.data.violationcorrection.UpdateLinkLanguageCorrection;
+import fr.mazure.homepagemanager.data.violationcorrection.ViolationCorrection;
+
+/**
+ * Test of UpdateLinkLanguageCorrection class
+ */
+public class UpdateLinkLanguageCorrectionTest {
+
+
+    @SuppressWarnings("static-method")
+    @Test
+    void languageIsUpdated() {
+
+        final String content =
+            "<?xml version=\"1.0\"?>\r\n" +
+            "<?xml-stylesheet type=\"text/xsl\" href=\"../css/strict.xsl\"?>\r\n" +
+            "<PAGE xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:noNamespaceSchemaLocation=\"../css/schema.xsd\">\r\n" +
+            "<TITLE>test</TITLE>\r\n" +
+            "<PATH>dummy-dir/test.xml</PATH>\r\n" +
+            "<DATE><YEAR>2016</YEAR><MONTH>1</MONTH><DAY>30</DAY></DATE>\r\n" +
+            "<CONTENT>\r\n" +
+            "<BLIST><TITLE>Articles and videos</TITLE>\r\n" +
+            "<ITEM><ARTICLE><X><T>Visual Perception with Deep Learning</T><A>https://www.youtube.com/watch?v=3boKlkPBckA</A><L>fr</L><F>MP4</F><DURATION><MINUTE>57</MINUTE><SECOND>25</SECOND></DURATION></X><AUTHOR><FIRSTNAME>Yann</FIRSTNAME><LASTNAME>Le Cun</LASTNAME></AUTHOR><DATE><YEAR>2008</YEAR><MONTH>4</MONTH><DAY>10</DAY></DATE><COMMENT>Machine Learning: a description of the structure and learning methodology of a deep multi-layered architecture. Yann gives some examples for real-time video analysis.</COMMENT></ARTICLE></ITEM>\r\n" +
+            "</BLIST>\r\n" +
+            "</CONTENT>\r\n" +
+            "</PAGE>";
+
+        final String expected =
+            "<?xml version=\"1.0\"?>\r\n" +
+            "<?xml-stylesheet type=\"text/xsl\" href=\"../css/strict.xsl\"?>\r\n" +
+            "<PAGE xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:noNamespaceSchemaLocation=\"../css/schema.xsd\">\r\n" +
+            "<TITLE>test</TITLE>\r\n" +
+            "<PATH>dummy-dir/test.xml</PATH>\r\n" +
+            "<DATE><YEAR>2016</YEAR><MONTH>1</MONTH><DAY>30</DAY></DATE>\r\n" +
+            "<CONTENT>\r\n" +
+            "<BLIST><TITLE>Articles and videos</TITLE>\r\n" +
+            "<ITEM><ARTICLE><X><T>Visual Perception with Deep Learning</T><A>https://www.youtube.com/watch?v=3boKlkPBckA</A><L>en</L><F>MP4</F><DURATION><MINUTE>57</MINUTE><SECOND>25</SECOND></DURATION></X><AUTHOR><FIRSTNAME>Yann</FIRSTNAME><LASTNAME>Le Cun</LASTNAME></AUTHOR><DATE><YEAR>2008</YEAR><MONTH>4</MONTH><DAY>10</DAY></DATE><COMMENT>Machine Learning: a description of the structure and learning methodology of a deep multi-layered architecture. Yann gives some examples for real-time video analysis.</COMMENT></ARTICLE></ITEM>\r\n" +
+            "</BLIST>\r\n" +
+            "</CONTENT>\r\n" +
+            "</PAGE>";
+
+        final ViolationCorrection correction = new UpdateLinkLanguageCorrection(Locale.FRENCH,
+                                                                                Locale.ENGLISH,
+                                                                                "https://www.youtube.com/watch?v=3boKlkPBckA");
+        Assertions.assertEquals(expected, correction.apply(content));
+    }
+
+}
