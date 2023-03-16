87c826a65bc4b1d160b15b3c2b2722b630b69014
Laurent <Laurent@gilgamesh.home>
2023-03-16 08:13:42 +0100

    Update of URL when used as predecessor

--- src/main/java/fr/mazure/homepagemanager/data/violationcorrection/UpdateLinkUrlCorrection.java
+++ src/main/java/fr/mazure/homepagemanager/data/violationcorrection/UpdateLinkUrlCorrection.java
@@ -1,11 +1,17 @@
 package fr.mazure.homepagemanager.data.violationcorrection;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
  * Correct the URL of a link
  */
-public class UpdateLinkUrlCorrection  extends RegexpViolationCorrection {
+public class UpdateLinkUrlCorrection extends ViolationCorrection {
+
+    private final Pattern _pattern1;
+    private final String _replacement1;
+    private final Pattern _pattern2;
+    private final String _replacement2;
 
     /**
      * Constructor
@@ -14,8 +20,18 @@ public class UpdateLinkUrlCorrection  extends RegexpViolationCorrection {
      */
     public UpdateLinkUrlCorrection(final String badUrl,
                                    final String correctUrl) {
-        super("Update the link URL",
-              "<A>" + Pattern.quote(badUrl) + "</A>",
-              "<A>" + correctUrl + "</A>");
+        super("Update the link URL");
+        _pattern1 = Pattern.compile("<A>" + Pattern.quote(badUrl) + "</A>");
+        _replacement1 = "<A>" + correctUrl + "</A>";
+        _pattern2 = Pattern.compile("predecessor=\""  + Pattern.quote(badUrl) + "\"");
+        _replacement2 = "predecessor=\""  + correctUrl + "\"";
+    }
+
+    @Override
+    public String apply(String content) {
+        final Matcher matcher1 = _pattern1.matcher(content);
+        final String str =  matcher1.replaceAll(_replacement1);
+        final Matcher matcher2 = _pattern2.matcher(str);
+        return matcher2.replaceAll(_replacement2);
     }
 }
