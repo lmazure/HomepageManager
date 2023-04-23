31f9fe48623f88ee82d9ec7ebca7f8332d3d039d
Laurent <Laurent@gilgamesh.home>
2023-04-23 08:49:49 +0200

    Improved management of incorrect redirection URL

--- src/main/java/fr/mazure/homepagemanager/utils/internet/UriHelper.java
+++ src/main/java/fr/mazure/homepagemanager/utils/internet/UriHelper.java
@@ -1,7 +1,9 @@
 package fr.mazure.homepagemanager.utils.internet;
 
+import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
 
 import fr.mazure.homepagemanager.utils.ExitHelper;
 
@@ -19,8 +21,28 @@ public class UriHelper {
     public static URI convertStringToUri(final String str) {
         try {
             return new URI(str);
+        } catch (@SuppressWarnings("unused") final URISyntaxException e) {
+            return convertStringToUriSlowerButSafer(str);
+        }
+    }
+
+    /**
+     * Convert a string to URI, exit if the string is an invalid URI
+     * This version is slow since it uses the URL class
+     *
+     * @param str string
+     * @return URI
+     */
+    private static URI convertStringToUriSlowerButSafer(final String str) {
+        try {
+            final URL url = new URL(str);
+            return new URI(url.getProtocol(), url.getUserInfo(), url.getHost(), url.getPort(), url.getPath(), url.getQuery(), url.getRef());
         } catch (final URISyntaxException e) {
             ExitHelper.exit("Invalid URI", e);
+            // NOTREACHED
+            return null;
+        } catch (MalformedURLException e) {
+            ExitHelper.exit("Invalid URL", e);
             // NOTREACHED
             return null;
         }
@@ -29,10 +51,10 @@ public class UriHelper {
     /**
      * Build an URI from its components
      *
-     * @param scheme
-     * @param host
-     * @param path
-     * @return
+     * @param scheme scheme
+     * @param host host
+     * @param path path
+     * @return the built URI
      */
     public static URI buildUri(final String scheme,
                                final String host,
