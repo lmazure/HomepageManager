9b1307a3ac8b89b50483849775789d12da513742
Laurent <Laurent@gilgamesh.home>
2023-07-23 11:13:01 +0200

    Removed old code

--- src/main/java/fr/mazure/homepagemanager/data/linkchecker/LinkStatusAnalyzer.java
+++ src/main/java/fr/mazure/homepagemanager/data/linkchecker/LinkStatusAnalyzer.java
@@ -1,15 +1,11 @@
 package fr.mazure.homepagemanager.data.linkchecker;
 
-import java.net.HttpURLConnection;
-import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 import fr.mazure.homepagemanager.data.internet.FullFetchedLinkData;
 import fr.mazure.homepagemanager.data.internet.HeaderFetchedLinkData;
 import fr.mazure.homepagemanager.data.internet.SynchronousSiteDataRetriever;
 import fr.mazure.homepagemanager.data.linkchecker.linkstatusanalyzer.RedirectionData;
-import fr.mazure.homepagemanager.utils.internet.HttpHelper;
 import fr.mazure.homepagemanager.utils.xmlparsing.LinkData;
 import fr.mazure.homepagemanager.utils.xmlparsing.LinkStatus;
 
@@ -25,61 +21,10 @@ public class LinkStatusAnalyzer {
      * @param effectiveData data as retrieved from internet
      * @return true if and only if effectiveData matches expectedData
      */
-    public static boolean doesEffectiveDataMatchesExpectedData2(final LinkData expectedData,
-                                                               final FullFetchedLinkData effectiveData) {
-
-        if ((expectedData.getStatus() != LinkStatus.OK) && expectedData.getStatus() != LinkStatus.DEAD) {
-            if (effectiveData.error().isPresent()) {
-                return true;
-            }
-            if (!httpRequestIsSuccessful(effectiveData.headers().get())) {
-                return true;
-            }
-            final HeaderFetchedLinkData lastRedirection = lastRedirection(effectiveData);
-            if (lastRedirection == null) {
-                return false;
-            }
-            if (!httpRequestIsSuccessful(lastRedirection.headers().get())) {
-                return true;
-            }
-            return false;
-        }
-
-        final HeaderFetchedLinkData lastRedirection = lastRedirection(effectiveData);
-        return (effectiveData.headers().isPresent() &&
-                httpRequestIsSuccessful(effectiveData.headers().get()) &&
-                ((lastRedirection == null) ||
-                 lastRedirection.headers().isPresent() &&
-                 httpRequestIsSuccessful(lastRedirection.headers().get())));
-    }
-
-    /**
-     * @param expectedData data as expected in the XML file
-     * @param effectiveData data as retrieved from internet
-     * @return true if and only if effectiveData matches expectedData
-     */
     public static boolean doesEffectiveDataMatchesExpectedData(final LinkData expectedData,
                                                                final FullFetchedLinkData effectiveData) {
         final Set<LinkStatus> expectedStatuses = getPossibleStatuses(effectiveData);
         return expectedStatuses.contains(expectedData.getStatus());
-    }
-
-    private static HeaderFetchedLinkData lastRedirection(final FullFetchedLinkData data) {
-        HeaderFetchedLinkData d = data.previousRedirection();
-        if (d == null) {
-            return null;
-        }
-        while (d.previousRedirection() != null) {
-            d = d.previousRedirection();
-        }
-        return d;
-    }
-
-    private static boolean httpRequestIsSuccessful(final Map<String, List<String>> headers) {
-        final int code = HttpHelper.getResponseCodeFromHeaders(headers);
-        return (code == HttpURLConnection.HTTP_OK) ||
-               (code == HttpURLConnection.HTTP_MOVED_TEMP) ||
-               (code == HttpURLConnection.HTTP_SEE_OTHER);
     }
 
     private static Set<LinkStatus> getPossibleStatuses(final FullFetchedLinkData effectiveData) {
@@ -98,6 +43,5 @@ public class LinkStatusAnalyzer {
         }
         return n;
     }
-
 }
 
