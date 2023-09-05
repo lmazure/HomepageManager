1cbde741ea2080e0fee71e5014fc8be9350ca321
Laurent Mazuré <lmazure@henix.fr>
2023-09-05 07:41:06 +0200

    Remove dead code fr.mazure.homepagemanager.utils.JsonHelper

--- src/test/java/fr/mazure/homepagemanager/data/nodechecker/test/BritishCheckerTest.java
+++ src/test/java/fr/mazure/homepagemanager/data/nodechecker/test/BritishCheckerTest.java
@@ -23,8 +23,8 @@ public class BritishCheckerTest extends NodeValueCheckerTestBase {
             "<DATE><YEAR>2016</YEAR><MONTH>1</MONTH><DAY>30</DAY></DATE>\n" +
             "<CONTENT>\n" +
             "<BLIST>\n" +
-            "<ITEM><ARTICLE><X><T>When are two strings equal?</T><A>http://java.sun.com</A><L>en</L><F>HTML</F></X><COMMENT>A quick introduction to the common string pool and the literal strings consolidation in a same package.</COMMENT></ARTICLE></ITEM>\n" +
-            "<ITEM><ARTICLE><X><T>When are two strings equal?</T><A>http://java.sun.com</A><L>en</L><F>HTML</F></X><COMMENT>M102 was improperly recorded in Messier catalog, it is a galaxy seen almost exactly edge-on.</COMMENT></ARTICLE></ITEM>\\n" +
+            "<ITEM><ARTICLE><X protection=\"free_registration\"><T>When are two strings equal?</T><A>http://java.sun.com</A><L>en</L><F>HTML</F></X><COMMENT>A quick introduction to the common string pool and the literal strings consolidation in a same package.</COMMENT></ARTICLE></ITEM>\n" +
+            "<ITEM><ARTICLE><X status=\"zombie\"><T>When are two strings equal?</T><A>http://java.sun.com</A><L>en</L><F>HTML</F></X><COMMENT>M102 was improperly recorded in Messier catalog, it is a galaxy seen almost exactly edge-on.</COMMENT></ARTICLE></ITEM>\\n" +
             "<ITEM><ARTICLE><X><T>When are two strings equal?</T><A>http://java.sun.com</A><L>en</L><F>HTML</F></X><COMMENT>Evaluating the magnetic dipole moment of neutrinos with the color–magnitude diagram of M5.</COMMENT></ARTICLE></ITEM>\n" +
             "<ITEM><ARTICLE><X><T>When are two strings equal?</T><A>http://java.sun.com</A><L>en</L><F>HTML</F></X><COMMENT>Some samples stored in the Oak Ridge National Laboratory.</COMMENT></ARTICLE></ITEM>\n" +
             "<ITEM><ARTICLE><X><T>When are two strings equal?</T><A>http://java.sun.com</A><L>en</L><F>HTML</F></X><COMMENT>The production of Californium at the Oak Ridge National Laboratory and an experiment with it.</COMMENT></ARTICLE></ITEM>\n" +
