128b727ca4afab534d29354fba9a83ad2d665687
Laurent <laurent@gilgamesh>
2024-10-06 14:15:14 +0200

    Chore: continue at reducing the amount of UT code

--- src/test/java/fr/mazure/homepagemanager/data/linkchecker/arstechnica/test/ArsTechnicaLinkContentParserTest.java
+++ src/test/java/fr/mazure/homepagemanager/data/linkchecker/arstechnica/test/ArsTechnicaLinkContentParserTest.java
@@ -1,21 +1,10 @@
 package fr.mazure.homepagemanager.data.linkchecker.arstechnica.test;
 
-import java.util.Collections;
-import java.util.Optional;
-import java.util.concurrent.atomic.AtomicBoolean;
-
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import fr.mazure.homepagemanager.data.dataretriever.FullFetchedLinkData;
-import fr.mazure.homepagemanager.data.dataretriever.SynchronousSiteDataRetriever;
-import fr.mazure.homepagemanager.data.dataretriever.test.TestHelper;
-import fr.mazure.homepagemanager.data.linkchecker.ContentParserException;
 import fr.mazure.homepagemanager.data.linkchecker.arstechnica.ArsTechnicaLinkContentParser;
 import fr.mazure.homepagemanager.data.linkchecker.test.LinkDataExtractorTestBase;
-import fr.mazure.homepagemanager.utils.internet.HtmlHelper;
-import fr.mazure.homepagemanager.utils.xmlparsing.AuthorData;
 
 /**
  * Tests of ArsTechnicaLinkContentParser
@@ -45,6 +34,7 @@ class ArsTechnicaLinkContentParserTest extends LinkDataExtractorTestBase {
         checkSubtitle(ArsTechnicaLinkContentParser.class, url, expectedSubtitle);
     }
 
+    @SuppressWarnings("static-method")
     @ParameterizedTest
     @CsvSource({
         "https://arstechnica.com/cars/2021/04/consumer-reports-shows-tesla-autopilot-works-with-no-one-in-the-drivers-seat/,2021-04-22",
@@ -52,25 +42,10 @@ class ArsTechnicaLinkContentParserTest extends LinkDataExtractorTestBase {
         })
     void testDate(final String url,
                   final String expectedDate) {
-        final SynchronousSiteDataRetriever retriever = TestHelper.buildDataSiteRetriever(getClass());
-        final AtomicBoolean consumerHasBeenCalled = new AtomicBoolean(false);
-        retriever.retrieve(url,
-                           (final Boolean b, final FullFetchedLinkData d) -> {
-                               Assertions.assertTrue(d.dataFileSection().isPresent());
-                               final String data = HtmlHelper.slurpFile(d.dataFileSection().get());
-                               final ArsTechnicaLinkContentParser parser = new ArsTechnicaLinkContentParser(url, data);
-                               try {
-                                   Assertions.assertTrue(parser.getDate().isPresent());
-                                   Assertions.assertEquals(expectedDate, parser.getDate().get().toString());
-                                } catch (final ContentParserException e) {
-                                    Assertions.fail("getDate threw " + e.getMessage());
-                                }
-                               consumerHasBeenCalled.set(true);
-                           },
-                           false);
-        Assertions.assertTrue(consumerHasBeenCalled.get());
+        checkDate(ArsTechnicaLinkContentParser.class, url, expectedDate);
     }
 
+    @SuppressWarnings("static-method")
     @ParameterizedTest
     @CsvSource({
         "https://arstechnica.com/cars/2021/04/consumer-reports-shows-tesla-autopilot-works-with-no-one-in-the-drivers-seat/,Timothy,B.,Lee",
@@ -93,30 +68,18 @@ class ArsTechnicaLinkContentParserTest extends LinkDataExtractorTestBase {
                     final String expectedFirstName,
                     final String expectedMiddleName,
                     final String expectedLastName) {
-        final AuthorData expectedAuthor = new AuthorData(Optional.empty(),
-                                                         Optional.of(expectedFirstName),
-                                                         Optional.ofNullable(expectedMiddleName),
-                                                         Optional.of(expectedLastName),
-                                                         Optional.empty(),
-                                                         Optional.empty());
-        final SynchronousSiteDataRetriever retriever = TestHelper.buildDataSiteRetriever(getClass());
-        final AtomicBoolean consumerHasBeenCalled = new AtomicBoolean(false);
-        retriever.retrieve(url,
-                           (final Boolean b, final FullFetchedLinkData d) -> {
-                               Assertions.assertTrue(d.dataFileSection().isPresent());
-                               final String data = HtmlHelper.slurpFile(d.dataFileSection().get());
-                               final ArsTechnicaLinkContentParser parser = new ArsTechnicaLinkContentParser(url, data);
-                               try {
-                                   Assertions.assertEquals(Collections.singletonList(expectedAuthor), parser.getSureAuthors());
-                                } catch (final ContentParserException e) {
-                                    Assertions.fail("getSureAuthors threw " + e.getMessage());
-                                }
-                               consumerHasBeenCalled.set(true);
-                           },
-                           false);
-        Assertions.assertTrue(consumerHasBeenCalled.get());
+
+        check1Author(ArsTechnicaLinkContentParser.class,
+                     url,
+                     null,
+                     expectedFirstName,
+                     expectedMiddleName,
+                     expectedLastName,
+                     null,
+                     null);
     }
 
+    @SuppressWarnings("static-method")
     @ParameterizedTest
     @CsvSource({
         "https://arstechnica.com/information-technology/2023/03/chinese-search-giant-launches-ai-chatbot-with-prerecorded-demo/,Ryan,,McMorrow,Qianer,,Liu",
@@ -128,38 +91,23 @@ class ArsTechnicaLinkContentParserTest extends LinkDataExtractorTestBase {
                       final String expectedFirstName2,
                       final String expectedMiddleName2,
                       final String expectedLastName2) {
-        final AuthorData expectedAuthor1 = new AuthorData(Optional.empty(),
-                                                          Optional.of(expectedFirstName1),
-                                                          Optional.ofNullable(expectedMiddleName1),
-                                                          Optional.of(expectedLastName1),
-                                                          Optional.empty(),
-                                                          Optional.empty());
-        final AuthorData expectedAuthor2 = new AuthorData(Optional.empty(),
-                                                          Optional.of(expectedFirstName2),
-                                                          Optional.ofNullable(expectedMiddleName2),
-                                                          Optional.of(expectedLastName2),
-                                                          Optional.empty(),
-                                                          Optional.empty());
-        final SynchronousSiteDataRetriever retriever = TestHelper.buildDataSiteRetriever(getClass());
-        final AtomicBoolean consumerHasBeenCalled = new AtomicBoolean(false);
-        retriever.retrieve(url,
-                           (final Boolean b, final FullFetchedLinkData d) -> {
-                               Assertions.assertTrue(d.dataFileSection().isPresent());
-                               final String data = HtmlHelper.slurpFile(d.dataFileSection().get());
-                               final ArsTechnicaLinkContentParser parser = new ArsTechnicaLinkContentParser(url, data);
-                               try {
-                                   Assertions.assertEquals(2, parser.getSureAuthors().size());
-                                   Assertions.assertEquals(expectedAuthor1, parser.getSureAuthors().get(0));
-                                   Assertions.assertEquals(expectedAuthor2, parser.getSureAuthors().get(1));
-                                } catch (final ContentParserException e) {
-                                    Assertions.fail("getSureAuthors threw " + e.getMessage());
-                                }
-                               consumerHasBeenCalled.set(true);
-                           },
-                           false);
-        Assertions.assertTrue(consumerHasBeenCalled.get());
+        check2Authors(ArsTechnicaLinkContentParser.class,
+                      url,
+                      null,
+                      expectedFirstName1,
+                      expectedMiddleName1,
+                      expectedLastName1,
+                      null,
+                      null,
+                      null,
+                      expectedFirstName2,
+                      expectedMiddleName2,
+                      expectedLastName2,
+                      null,
+                      null);
     }
 
+    @SuppressWarnings("static-method")
     @ParameterizedTest
     @CsvSource({
         "https://arstechnica.com/tech-policy/2022/09/texts-show-roll-call-of-tech-figures-tried-to-help-elon-musk-in-twitter-deal/,Hannah,,Murphy,James,,Fontanella-Khan,Sujeet,,Indap",
@@ -175,65 +123,35 @@ class ArsTechnicaLinkContentParserTest extends LinkDataExtractorTestBase {
                       final String expectedFirstName3,
                       final String expectedMiddleName3,
                       final String expectedLastName3) {
-        final AuthorData expectedAuthor1 = new AuthorData(Optional.empty(),
-                                                          Optional.of(expectedFirstName1),
-                                                          Optional.ofNullable(expectedMiddleName1),
-                                                          Optional.of(expectedLastName1),
-                                                          Optional.empty(),
-                                                          Optional.empty());
-        final AuthorData expectedAuthor2 = new AuthorData(Optional.empty(),
-                                                          Optional.of(expectedFirstName2),
-                                                          Optional.ofNullable(expectedMiddleName2),
-                                                          Optional.of(expectedLastName2),
-                                                          Optional.empty(),
-                                                          Optional.empty());
-        final AuthorData expectedAuthor3 = new AuthorData(Optional.empty(),
-                                                          Optional.of(expectedFirstName3),
-                                                          Optional.ofNullable(expectedMiddleName3),
-                                                          Optional.of(expectedLastName3),
-                                                          Optional.empty(),
-                                                          Optional.empty());
-        final SynchronousSiteDataRetriever retriever = TestHelper.buildDataSiteRetriever(getClass());
-        final AtomicBoolean consumerHasBeenCalled = new AtomicBoolean(false);
-        retriever.retrieve(url,
-                           (final Boolean b, final FullFetchedLinkData d) -> {
-                               Assertions.assertTrue(d.dataFileSection().isPresent());
-                               final String data = HtmlHelper.slurpFile(d.dataFileSection().get());
-                               final ArsTechnicaLinkContentParser parser = new ArsTechnicaLinkContentParser(url, data);
-                               try {
-                                   Assertions.assertEquals(3, parser.getSureAuthors().size());
-                                   Assertions.assertEquals(expectedAuthor1, parser.getSureAuthors().get(0));
-                                   Assertions.assertEquals(expectedAuthor2, parser.getSureAuthors().get(1));
-                                   Assertions.assertEquals(expectedAuthor3, parser.getSureAuthors().get(2));
-                                } catch (final ContentParserException e) {
-                                    Assertions.fail("getSureAuthors threw " + e.getMessage());
-                                }
-                               consumerHasBeenCalled.set(true);
-                           },
-                           false);
-        Assertions.assertTrue(consumerHasBeenCalled.get());
+        check3Authors(ArsTechnicaLinkContentParser.class,
+                      url,
+                      null,
+                      expectedFirstName1,
+                      expectedMiddleName1,
+                      expectedLastName1,
+                      null,
+                      null,
+                      null,
+                      expectedFirstName2,
+                      expectedMiddleName2,
+                      expectedLastName2,
+                      null,
+                      null,
+                      null,
+                      expectedFirstName3,
+                      expectedMiddleName3,
+                      expectedLastName3,
+                      null,
+                      null);
     }
 
+    @SuppressWarnings("static-method")
     @ParameterizedTest
     @CsvSource({
         "https://arstechnica.com/information-technology/2012/03/microsoft-announces-cloud-building-with-tfs-feature-packs-for-visual-studio/",
         })
     void testAuthorAbsence(final String url) {
-        final SynchronousSiteDataRetriever retriever = TestHelper.buildDataSiteRetriever(getClass());
-        final AtomicBoolean consumerHasBeenCalled = new AtomicBoolean(false);
-        retriever.retrieve(url,
-                           (final Boolean b, final FullFetchedLinkData d) -> {
-                               Assertions.assertTrue(d.dataFileSection().isPresent());
-                               final String data = HtmlHelper.slurpFile(d.dataFileSection().get());
-                               final ArsTechnicaLinkContentParser parser = new ArsTechnicaLinkContentParser(url, data);
-                               try {
-                                   Assertions.assertEquals(0, parser.getSureAuthors().size());
-                                } catch (final ContentParserException e) {
-                                    Assertions.fail("getSureAuthors threw " + e.getMessage());
-                                }
-                               consumerHasBeenCalled.set(true);
-                           },
-                           false);
-        Assertions.assertTrue(consumerHasBeenCalled.get());
+        check0Author(ArsTechnicaLinkContentParser.class,
+                     url);
     }
 }
--- src/test/java/fr/mazure/homepagemanager/data/linkchecker/test/LinkDataExtractorTestBase.java
+++ src/test/java/fr/mazure/homepagemanager/data/linkchecker/test/LinkDataExtractorTestBase.java
@@ -2,6 +2,9 @@ package fr.mazure.homepagemanager.data.linkchecker.test;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Consumer;
 
@@ -13,6 +16,7 @@ import fr.mazure.homepagemanager.data.dataretriever.test.TestHelper;
 import fr.mazure.homepagemanager.data.linkchecker.ContentParserException;
 import fr.mazure.homepagemanager.data.linkchecker.LinkDataExtractor;
 import fr.mazure.homepagemanager.utils.internet.HtmlHelper;
+import fr.mazure.homepagemanager.utils.xmlparsing.AuthorData;
 
 /**
  * Base class for the LinkDataExtractor tests
@@ -49,6 +53,135 @@ public class LinkDataExtractorTestBase {
                     });
     }
 
+
+    protected static void checkDate(final Class<? extends LinkDataExtractor> clazz,
+                                    final String url,
+                                    final String expectedDate) {
+        perform(clazz,
+                url,
+                (LinkDataExtractor p) ->
+                    {
+                        try {
+                            Assertions.assertTrue(p.getDate().isPresent());
+                            Assertions.assertEquals(expectedDate, p.getDate().get().toString());
+                        } catch (final ContentParserException e) {
+                            Assertions.fail("getTitle threw " + e.getMessage());
+                        }
+                    });
+    }
+
+    protected static void check0Author(final Class<? extends LinkDataExtractor> clazz,
+                                       final String url) {
+        final List<AuthorData> expectedAuthors = new ArrayList<>();
+        checkAuthors(clazz, url, expectedAuthors);
+    }
+
+    protected static void check1Author(final Class<? extends LinkDataExtractor> clazz,
+                                       final String url,
+                                       final String expectedNamePrefix,
+                                       final String expectedFirstName,
+                                       final String expectedMiddleName,
+                                       final String expectedLastName,
+                                       final String expectedNameSuffix,
+                                       final String expectedGivenName) {
+        final List<AuthorData> expectedAuthors = new ArrayList<>();
+        expectedAuthors.add(new AuthorData(Optional.ofNullable(expectedNamePrefix),
+                                           Optional.ofNullable(expectedFirstName),
+                                           Optional.ofNullable(expectedMiddleName),
+                                           Optional.ofNullable(expectedLastName),
+                                           Optional.ofNullable(expectedNameSuffix),
+                                           Optional.ofNullable(expectedGivenName)));
+        checkAuthors(clazz, url, expectedAuthors);
+   
+   }
+
+    protected static void check2Authors(final Class<? extends LinkDataExtractor> clazz,
+                                        final String url,
+                                        final String expectedNamePrefix1,
+                                        final String expectedFirstName1,
+                                        final String expectedMiddleName1,
+                                        final String expectedLastName1,
+                                        final String expectedNameSuffix1,
+                                        final String expectedGivenName1,
+                                        final String expectedNamePrefix2,
+                                        final String expectedFirstName2,
+                                        final String expectedMiddleName2,
+                                        final String expectedLastName2,
+                                        final String expectedNameSuffix2,
+                                        final String expectedGivenName2) {
+        final List<AuthorData> expectedAuthors = new ArrayList<>();
+        expectedAuthors.add(new AuthorData(Optional.ofNullable(expectedNamePrefix1),
+                                                            Optional.ofNullable(expectedFirstName1),
+                                                            Optional.ofNullable(expectedMiddleName1),
+                                                            Optional.ofNullable(expectedLastName1),
+                                                            Optional.ofNullable(expectedNameSuffix1),
+                                                            Optional.ofNullable(expectedGivenName1)));
+        expectedAuthors.add(new AuthorData(Optional.ofNullable(expectedNamePrefix2),
+                                                            Optional.ofNullable(expectedFirstName2),
+                                                            Optional.ofNullable(expectedMiddleName2),
+                                                            Optional.ofNullable(expectedLastName2),
+                                                            Optional.ofNullable(expectedNameSuffix2),
+                                                            Optional.ofNullable(expectedGivenName2)));
+        checkAuthors(clazz, url, expectedAuthors);
+    }
+
+    protected static void check3Authors(final Class<? extends LinkDataExtractor> clazz,
+                                        final String url,
+                                        final String expectedNamePrefix1,
+                                        final String expectedFirstName1,
+                                        final String expectedMiddleName1,
+                                        final String expectedLastName1,
+                                        final String expectedNameSuffix1,
+                                        final String expectedGivenName1,
+                                        final String expectedNamePrefix2,
+                                        final String expectedFirstName2,
+                                        final String expectedMiddleName2,
+                                        final String expectedLastName2,
+                                        final String expectedGivenName2,
+                                        final String expectedNameSuffix2,
+                                        final String expectedNamePrefix3,
+                                        final String expectedFirstName3,
+                                        final String expectedMiddleName3,
+                                        final String expectedLastName3,
+                                        final String expectedNameSuffix3,
+                                        final String expectedGivenName3) {
+        final List<AuthorData> expectedAuthors = new ArrayList<>();
+        expectedAuthors.add(new AuthorData(Optional.ofNullable(expectedNamePrefix1),
+                                                          Optional.ofNullable(expectedFirstName1),
+                                                          Optional.ofNullable(expectedMiddleName1),
+                                                          Optional.ofNullable(expectedLastName1),
+                                                          Optional.ofNullable(expectedNameSuffix1),
+                                                          Optional.ofNullable(expectedGivenName1)));
+        expectedAuthors.add(new AuthorData(Optional.ofNullable(expectedNamePrefix2),
+                                                          Optional.ofNullable(expectedFirstName2),
+                                                          Optional.ofNullable(expectedMiddleName2),
+                                                          Optional.ofNullable(expectedLastName2),
+                                                          Optional.ofNullable(expectedNameSuffix2),
+                                                          Optional.ofNullable(expectedGivenName2)));
+        expectedAuthors.add(new AuthorData(Optional.ofNullable(expectedNamePrefix3),
+                                                          Optional.ofNullable(expectedFirstName3),
+                                                          Optional.ofNullable(expectedMiddleName3),
+                                                          Optional.ofNullable(expectedLastName3),
+                                                          Optional.ofNullable(expectedNameSuffix3),
+                                                          Optional.ofNullable(expectedGivenName3)));
+        checkAuthors(clazz, url, expectedAuthors);
+    }
+
+    private static void checkAuthors(final Class<? extends LinkDataExtractor> clazz,
+                                     final String url,
+                                     final List<AuthorData> expectedAuthors) {
+        perform(clazz,
+                url,
+                (LinkDataExtractor p) ->
+                    {
+                        try {
+                            Assertions.assertEquals(expectedAuthors, p.getSureAuthors());
+                        } catch (final ContentParserException e) {
+                            Assertions.fail("getSureAuthors threw " + e.getMessage());
+                        }
+                    });
+    }
+
     protected static void perform(final Class<? extends LinkDataExtractor> clazz,
                                   final String url,
                                   final Consumer<LinkDataExtractor> assertor) {
@@ -70,6 +203,7 @@ public class LinkDataExtractorTestBase {
                                                final String url,
                                                final String data) {
         try {
+            @SuppressWarnings("unchecked")
             final Constructor<LinkDataExtractor> constructor = (Constructor<LinkDataExtractor>)clazz.getConstructor(String.class, String.class);
             return constructor.newInstance(url, data);
         } catch (final InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException | NoSuchMethodException | SecurityException e) {
