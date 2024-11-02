89f6a14065fc9347a836a32145526f84edebeb31
Laurent <laurent@gilgamesh>
2024-11-02 22:31:56 +0100

    Chore: clean up code

--- src/test/java/fr/mazure/homepagemanager/data/linkchecker/spectrum/test/SpectrumLinkContentParserTest.java
+++ src/test/java/fr/mazure/homepagemanager/data/linkchecker/spectrum/test/SpectrumLinkContentParserTest.java
@@ -1,99 +1,47 @@
 package fr.mazure.homepagemanager.data.linkchecker.spectrum.test;
 
-import java.util.Collections;
-import java.util.Optional;
-import java.util.concurrent.atomic.AtomicBoolean;
-
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import fr.mazure.homepagemanager.data.dataretriever.CachedSiteDataRetriever;
-import fr.mazure.homepagemanager.data.dataretriever.FullFetchedLinkData;
-import fr.mazure.homepagemanager.data.dataretriever.test.TestHelper;
-import fr.mazure.homepagemanager.data.linkchecker.ContentParserException;
 import fr.mazure.homepagemanager.data.linkchecker.spectrum.SpectrumLinkContentParser;
-import fr.mazure.homepagemanager.utils.internet.HtmlHelper;
-import fr.mazure.homepagemanager.utils.xmlparsing.AuthorData;
+import fr.mazure.homepagemanager.data.linkchecker.test.LinkDataExtractorTestBase;
 
 /**
  * Tests of SpectrumLinkContentParser
  */
-class SpectrumLinkContentParserTest {
+class SpectrumLinkContentParserTest extends LinkDataExtractorTestBase {
 
+    @SuppressWarnings("static-method")
     @ParameterizedTest
     @CsvSource(value = {
         "https://spectrum.ieee.org/intel-20a|In 2024, Intel Hopes to Leapfrog Its Chipmaking Competitors "
     }, delimiter = '|')
     void testTitle(final String url,
                    final String expectedTitle) {
-        final CachedSiteDataRetriever retriever = TestHelper.buildDataSiteRetriever(getClass());
-        final AtomicBoolean consumerHasBeenCalled = new AtomicBoolean(false);
-        retriever.retrieve(url,
-                           (final FullFetchedLinkData d) -> {
-                               Assertions.assertTrue(d.dataFileSection().isPresent());
-                               final String data = HtmlHelper.slurpFile(d.dataFileSection().get());
-                               final SpectrumLinkContentParser parser = new SpectrumLinkContentParser(url, data, retriever);
-                               try {
-                                   Assertions.assertEquals(expectedTitle, parser.getTitle());
-                               } catch (final ContentParserException e) {
-                                   Assertions.fail("getTitle threw " + e.getMessage());
-                               }
-                               consumerHasBeenCalled.set(true);
-                           },
-                           false);
-        Assertions.assertTrue(consumerHasBeenCalled.get());
+        checkTitle(SpectrumLinkContentParser.class, url, expectedTitle);
     }
 
+    @SuppressWarnings("static-method")
     @ParameterizedTest
     @CsvSource(value = {
         "https://spectrum.ieee.org/intel-20a|The chipmaker is betting on new transistors and power-delivery tech"
     }, delimiter = '|')
     void testSubtitle(final String url,
                       final String expectedSubtitle) {
-        final CachedSiteDataRetriever retriever = TestHelper.buildDataSiteRetriever(getClass());
-        final AtomicBoolean consumerHasBeenCalled = new AtomicBoolean(false);
-        retriever.retrieve(url,
-                           (final FullFetchedLinkData d) -> {
-                               Assertions.assertTrue(d.dataFileSection().isPresent());
-                               final String data = HtmlHelper.slurpFile(d.dataFileSection().get());
-                               final SpectrumLinkContentParser parser = new SpectrumLinkContentParser(url, data, retriever);
-                               try {
-                                   Assertions.assertEquals(expectedSubtitle, parser.getSubtitle().get());
-                               } catch (final ContentParserException e) {
-                                   Assertions.fail("getSubtitle threw " + e.getMessage());
-                               }
-                               consumerHasBeenCalled.set(true);
-                           },
-                           false);
-        Assertions.assertTrue(consumerHasBeenCalled.get());
+        checkSubtitle(SpectrumLinkContentParser.class, url, expectedSubtitle);
     }
 
+    @SuppressWarnings("static-method")
     @ParameterizedTest
     @CsvSource(value = {
         "https://spectrum.ieee.org/intel-20a|2023-12-18"
     }, delimiter = '|')
     void testDate(final String url,
                   final String expectedDate) {
-        final CachedSiteDataRetriever retriever = TestHelper.buildDataSiteRetriever(getClass());
-        final AtomicBoolean consumerHasBeenCalled = new AtomicBoolean(false);
-        retriever.retrieve(url,
-                           (final FullFetchedLinkData d) -> {
-                               Assertions.assertTrue(d.dataFileSection().isPresent());
-                               final String data = HtmlHelper.slurpFile(d.dataFileSection().get());
-                               final SpectrumLinkContentParser parser = new SpectrumLinkContentParser(url, data, retriever);
-                               try {
-                                   Assertions.assertTrue(parser.getDate().isPresent());
-                                   Assertions.assertEquals(expectedDate, parser.getDate().get().toString());
-                                } catch (final ContentParserException e) {
-                                    Assertions.fail("getDate threw " + e.getMessage());
-                                }
-                               consumerHasBeenCalled.set(true);
-                           },
-                           false);
-        Assertions.assertTrue(consumerHasBeenCalled.get());
+		checkDate(SpectrumLinkContentParser.class, url, expectedDate);
     }
 
+    @SuppressWarnings("static-method")
     @ParameterizedTest
     @CsvSource(value = {
         "https://spectrum.ieee.org/intel-20a|Gwendolyn||Rak|",
@@ -105,31 +53,17 @@ class SpectrumLinkContentParserTest {
                     final String expectedMiddleName,
                     final String expectedLastName,
                     final String expectedGivenName) {
-        final AuthorData expectedAuthor = new AuthorData(Optional.empty(),
-                                                         Optional.of(expectedFirstName),
-                                                         Optional.ofNullable(expectedMiddleName),
-                                                         Optional.of(expectedLastName),
-                                                         Optional.empty(),
-                                                         Optional.ofNullable(expectedGivenName));
-        final CachedSiteDataRetriever retriever = TestHelper.buildDataSiteRetriever(getClass());
-        final AtomicBoolean consumerHasBeenCalled = new AtomicBoolean(false);
-        retriever.retrieve(url,
-                           (final FullFetchedLinkData d) -> {
-                               Assertions.assertTrue(d.dataFileSection().isPresent());
-                               final String data = HtmlHelper.slurpFile(d.dataFileSection().get());
-                               final SpectrumLinkContentParser parser = new SpectrumLinkContentParser(url, data, retriever);
-                               try {
-                                   Assertions.assertEquals(1, parser.getSureAuthors().size());
-                                   Assertions.assertEquals(Collections.singletonList(expectedAuthor), parser.getSureAuthors());
-                                } catch (final ContentParserException e) {
-                                    Assertions.fail("getSureAuthors threw " + e.getMessage());
-                                }
-                               consumerHasBeenCalled.set(true);
-                           },
-                           false);
-        Assertions.assertTrue(consumerHasBeenCalled.get());
+        check1Author(SpectrumLinkContentParser.class,
+                     url,
+                     null,
+                     expectedFirstName,
+                     expectedMiddleName,
+                     expectedLastName,
+                     null,
+                     expectedGivenName);
     }
 
+    @SuppressWarnings("static-method")
     @ParameterizedTest
     @CsvSource(value = {
             "https://spectrum.ieee.org/winner-multicore-made-simple|Brian||Santo|Sally||Adee",
@@ -142,35 +76,21 @@ class SpectrumLinkContentParserTest {
                       final String expectedFirstName2,
                       final String expectedMiddleName2,
                       final String expectedLastName2) {
-        final AuthorData expectedAuthor1 = new AuthorData(Optional.empty(),
-                                                          Optional.of(expectedFirstName1),
-                                                          Optional.ofNullable(expectedMiddleName1),
-                                                          Optional.of(expectedLastName1),
-                                                          Optional.empty(),
-                                                          Optional.empty());
-        final AuthorData expectedAuthor2 = new AuthorData(Optional.empty(),
-                                                          Optional.of(expectedFirstName2),
-                                                          Optional.ofNullable(expectedMiddleName2),
-                                                          Optional.of(expectedLastName2),
-                                                          Optional.empty(),
-                                                          Optional.empty());
-        final CachedSiteDataRetriever retriever = TestHelper.buildDataSiteRetriever(getClass());
-        final AtomicBoolean consumerHasBeenCalled = new AtomicBoolean(false);
-        retriever.retrieve(url,
-                           (final FullFetchedLinkData d) -> {
-                               Assertions.assertTrue(d.dataFileSection().isPresent());
-                               final String data = HtmlHelper.slurpFile(d.dataFileSection().get());
-                               final SpectrumLinkContentParser parser = new SpectrumLinkContentParser(url, data, retriever);
-                               try {
-                                   Assertions.assertEquals(2, parser.getSureAuthors().size());
-                                   Assertions.assertEquals(expectedAuthor1, parser.getSureAuthors().get(0));
-                                   Assertions.assertEquals(expectedAuthor2, parser.getSureAuthors().get(1));
-                                } catch (final ContentParserException e) {
-                                    Assertions.fail("getSureAuthors threw " + e.getMessage());
-                                }
-                               consumerHasBeenCalled.set(true);
-                           },
-                           false);
-        Assertions.assertTrue(consumerHasBeenCalled.get());
+        check2Authors(SpectrumLinkContentParser.class,
+                      url,
+                      // author 1
+                      null,
+                      expectedFirstName1,
+                      expectedMiddleName1,
+                      expectedLastName1,
+                      null,
+                      null,
+                      // author 2
+                      null,
+                      expectedFirstName2,
+                      expectedMiddleName2,
+                      expectedLastName2,
+                      null,
+                      null);
     }
 }
--- src/test/java/fr/mazure/homepagemanager/data/linkchecker/stackoverflowblog/test/StackOverflowBlogLinkContentParserTest.java
+++ src/test/java/fr/mazure/homepagemanager/data/linkchecker/stackoverflowblog/test/StackOverflowBlogLinkContentParserTest.java
@@ -1,100 +1,48 @@
 package fr.mazure.homepagemanager.data.linkchecker.stackoverflowblog.test;
 
-import java.util.Collections;
-import java.util.Optional;
-import java.util.concurrent.atomic.AtomicBoolean;
-
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import fr.mazure.homepagemanager.data.dataretriever.CachedSiteDataRetriever;
-import fr.mazure.homepagemanager.data.dataretriever.FullFetchedLinkData;
-import fr.mazure.homepagemanager.data.dataretriever.test.TestHelper;
-import fr.mazure.homepagemanager.data.linkchecker.ContentParserException;
 import fr.mazure.homepagemanager.data.linkchecker.stackoverflowblog.StackOverflowBlogLinkContentParser;
-import fr.mazure.homepagemanager.utils.internet.HtmlHelper;
-import fr.mazure.homepagemanager.utils.xmlparsing.AuthorData;
+import fr.mazure.homepagemanager.data.linkchecker.test.LinkDataExtractorTestBase;
 
 /**
  * Tests of StackOverflowBlogLinkContentParser class
  *
  */
-class StackOverflowBlogLinkContentParserTest {
+class StackOverflowBlogLinkContentParserTest extends LinkDataExtractorTestBase {
 
+    @SuppressWarnings("static-method")
     @ParameterizedTest
     @CsvSource({
         "https://stackoverflow.blog/2023/01/24/ai-applications-open-new-security-vulnerabilities/,AI applications open new security vulnerabilities",
         })
     void testTitle(final String url,
                    final String expectedTitle) {
-        final CachedSiteDataRetriever retriever = TestHelper.buildDataSiteRetriever(getClass());
-        final AtomicBoolean consumerHasBeenCalled = new AtomicBoolean(false);
-        retriever.retrieve(url,
-                           (final FullFetchedLinkData d) -> {
-                               Assertions.assertTrue(d.dataFileSection().isPresent());
-                               final String data = HtmlHelper.slurpFile(d.dataFileSection().get());
-                               final StackOverflowBlogLinkContentParser parser = new StackOverflowBlogLinkContentParser(url, data, retriever);
-                               try {
-                                   Assertions.assertEquals(expectedTitle, parser.getTitle());
-                               } catch (final ContentParserException e) {
-                                   Assertions.fail("getTitle threw " + e.getMessage());
-                               }
-                               consumerHasBeenCalled.set(true);
-                           },
-                           false);
-        Assertions.assertTrue(consumerHasBeenCalled.get());
+        checkTitle(StackOverflowBlogLinkContentParser.class, url, expectedTitle);
     }
 
+    @SuppressWarnings("static-method")
     @ParameterizedTest
     @CsvSource({
         "https://stackoverflow.blog/2023/01/24/ai-applications-open-new-security-vulnerabilities/,Your ML model and AI-as-a-service apps might open new attack surfaces. Here's how to mitigate them.",
         })
     void testSubtitle(final String url,
                       final String expectedSubtitle) {
-        final CachedSiteDataRetriever retriever = TestHelper.buildDataSiteRetriever(getClass());
-        final AtomicBoolean consumerHasBeenCalled = new AtomicBoolean(false);
-        retriever.retrieve(url,
-                           (final FullFetchedLinkData d) -> {
-                               Assertions.assertTrue(d.dataFileSection().isPresent());
-                               final String data = HtmlHelper.slurpFile(d.dataFileSection().get());
-                               final StackOverflowBlogLinkContentParser parser = new StackOverflowBlogLinkContentParser(url, data, retriever);
-                               try {
-                                   Assertions.assertEquals(expectedSubtitle, parser.getSubtitle().get());
-                               } catch (final ContentParserException e) {
-                                   Assertions.fail("getSubtitle threw " + e.getMessage());
-                               }
-                               consumerHasBeenCalled.set(true);
-                           },
-                           false);
-        Assertions.assertTrue(consumerHasBeenCalled.get());
+        checkSubtitle(StackOverflowBlogLinkContentParser.class, url, expectedSubtitle);
     }
 
+    @SuppressWarnings("static-method")
     @ParameterizedTest
     @CsvSource({
         "https://stackoverflow.blog/2023/01/24/ai-applications-open-new-security-vulnerabilities/,2023-01-24",
         })
     void testDate(final String url,
                   final String expectedDate) {
-        final CachedSiteDataRetriever retriever = TestHelper.buildDataSiteRetriever(getClass());
-        final AtomicBoolean consumerHasBeenCalled = new AtomicBoolean(false);
-        retriever.retrieve(url,
-                           (final FullFetchedLinkData d) -> {
-                               Assertions.assertTrue(d.dataFileSection().isPresent());
-                               final String data = HtmlHelper.slurpFile(d.dataFileSection().get());
-                               final StackOverflowBlogLinkContentParser parser = new StackOverflowBlogLinkContentParser(url, data, retriever);
-                               try {
-                                   Assertions.assertTrue(parser.getDate().isPresent());
-                                   Assertions.assertEquals(expectedDate, parser.getDate().get().toString());
-                                } catch (final ContentParserException e) {
-                                    Assertions.fail("getDate threw " + e.getMessage());
-                                }
-                               consumerHasBeenCalled.set(true);
-                           },
-                           false);
-        Assertions.assertTrue(consumerHasBeenCalled.get());
+        checkDate(StackOverflowBlogLinkContentParser.class, url, expectedDate);
     }
 
+    @SuppressWarnings("static-method")
     @ParameterizedTest
     @CsvSource({
         "https://stackoverflow.blog/2023/01/24/ai-applications-open-new-security-vulnerabilities/,Taimur,,Ijlal,",
@@ -106,27 +54,13 @@ class StackOverflowBlogLinkContentParserTest {
                     final String expectedMiddleName,
                     final String expectedLastName,
                     final String expectedNameSuffix) {
-        final AuthorData expectedAuthor = new AuthorData(Optional.empty(),
-                                                         Optional.of(expectedFirstName),
-                                                         Optional.ofNullable(expectedMiddleName),
-                                                         Optional.of(expectedLastName),
-                                                         Optional.ofNullable(expectedNameSuffix),
-                                                         Optional.empty());
-        final CachedSiteDataRetriever retriever = TestHelper.buildDataSiteRetriever(getClass());
-        final AtomicBoolean consumerHasBeenCalled = new AtomicBoolean(false);
-        retriever.retrieve(url,
-                           (final FullFetchedLinkData d) -> {
-                               Assertions.assertTrue(d.dataFileSection().isPresent());
-                               final String data = HtmlHelper.slurpFile(d.dataFileSection().get());
-                               final StackOverflowBlogLinkContentParser parser = new StackOverflowBlogLinkContentParser(url, data, retriever);
-                               try {
-                                   Assertions.assertEquals(Collections.singletonList(expectedAuthor), parser.getSureAuthors());
-                                } catch (final ContentParserException e) {
-                                    Assertions.fail("getSureAuthors threw " + e.getMessage());
-                                }
-                               consumerHasBeenCalled.set(true);
-                           },
-                           false);
-        Assertions.assertTrue(consumerHasBeenCalled.get());
+        check1Author(StackOverflowBlogLinkContentParser.class,
+                     url,
+                     null,
+                     expectedFirstName,
+                     expectedMiddleName,
+                     expectedLastName,
+                     expectedNameSuffix,
+                     null);
     }
 }
