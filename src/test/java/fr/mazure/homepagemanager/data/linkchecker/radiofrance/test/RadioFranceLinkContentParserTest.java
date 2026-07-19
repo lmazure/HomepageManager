847016e0dd2e7a988aa5311c58a4fb8366a377b9
Laurent <laurent@gilgamesh>
2026-07-19 22:16:19 +0200

    Fix: Radio France unit tests

--- src/test/java/fr/mazure/homepagemanager/data/linkchecker/radiofrance/test/RadioFranceLinkContentParserTest.java
+++ src/test/java/fr/mazure/homepagemanager/data/linkchecker/radiofrance/test/RadioFranceLinkContentParserTest.java
@@ -14,11 +14,11 @@ class RadioFranceLinkContentParserTest extends LinkDataExtractorTestBase {
     @SuppressWarnings("static-method")
     @ParameterizedTest
     @CsvSource(value = {
-        "https://www.radiofrance.fr/franceinter/podcasts/affaires-sensibles/affaires-sensibles-du-mercredi-13-mai-2026-3938758|La Nuit tragique d'un Golden Boy, le mirage Pascal Jeandet",
+        "https://www.radiofrance.fr/franceinter/podcasts/affaires-sensibles/affaires-sensibles-du-mercredi-13-mai-2026-3938758|La Nuit tragique d’un Golden Boy, le mirage Pascal Jeandet",
         "https://www.radiofrance.fr/franceinter/podcasts/affaires-sensibles/affaires-sensibles-du-jeudi-30-mai-2024-3028690|Coupe de monde 1934 ou le triomphe du fascisme",
-        "https://www.radiofrance.fr/franceinter/podcasts/espions-une-histoire-vraie/marita-lorenz-l-agente-de-la-cia-envoyee-a-cuba-pour-empoisonner-son-ex-amant-fidel-castro-6825291|Marita Lorenz, l'agente de la CIA envoyée à Cuba pour empoisonner son ex-amant Fidel Castro",
+        "https://www.radiofrance.fr/franceinter/podcasts/espions-une-histoire-vraie/marita-lorenz-l-agente-de-la-cia-envoyee-a-cuba-pour-empoisonner-son-ex-amant-fidel-castro-6825291|Marita Lorenz, l’agente de la CIA envoyée à Cuba pour empoisonner son ex-amant Fidel Castro",
         "https://www.radiofrance.fr/franceinter/podcasts/espions-une-histoire-vraie/vadim-krasikov-le-tueur-prefere-de-poutine-4287237|Melita Norwood, vieille dame anglaise et ex-espionne de Staline",
-        "https://www.radiofrance.fr/franceculture/podcasts/les-grandes-traversees/naissance-d-un-prodige-6979092|Naissance d'un prodige",
+        "https://www.radiofrance.fr/franceculture/podcasts/les-grandes-traversees/naissance-d-un-prodige-6979092|Naissance d’un prodige",
         "https://www.radiofrance.fr/franceculture/podcasts/les-grandes-traversees/les-mots-qui-tuent-6008423|Les mots qui tuent",
         "https://www.radiofrance.fr/franceculture/podcasts/les-grandes-traversees/l-etoile-de-papier-1181737|L'étoile de papier",
     }, delimiter = '|')
@@ -30,13 +30,13 @@ class RadioFranceLinkContentParserTest extends LinkDataExtractorTestBase {
     @SuppressWarnings("static-method")
     @ParameterizedTest
     @CsvSource(value = {
-        "https://www.radiofrance.fr/franceinter/podcasts/affaires-sensibles/affaires-sensibles-du-mercredi-13-mai-2026-3938758|Aujourd'hui dans Affaires sensibles : la Nuit tragique d'un Golden Boy, le mirage Pascal Jeandet",
-        "https://www.radiofrance.fr/franceinter/podcasts/affaires-sensibles/affaires-sensibles-du-jeudi-30-mai-2024-3028690|Aujourd'hui dans Affaires sensibles, la Coupe du monde de football 1934 en Italie ou le triomphe du fascisme.",
+        "https://www.radiofrance.fr/franceinter/podcasts/affaires-sensibles/affaires-sensibles-du-mercredi-13-mai-2026-3938758|Aujourd’hui dans Affaires sensibles : la Nuit tragique d’un Golden Boy, le mirage Pascal Jeandet",
+        "https://www.radiofrance.fr/franceinter/podcasts/affaires-sensibles/affaires-sensibles-du-jeudi-30-mai-2024-3028690|Aujourd’hui dans Affaires sensibles, la Coupe du monde de football 1934 en Italie ou le triomphe du fascisme.",
         "https://www.radiofrance.fr/franceinter/podcasts/espions-une-histoire-vraie/marita-lorenz-l-agente-de-la-cia-envoyee-a-cuba-pour-empoisonner-son-ex-amant-fidel-castro-6825291|En 1959, cette jeune Allemande de 19 ans débarque à Cuba en pleine révolution et devient la maîtresse de Fidel Castro. Rentrée aux Etats Unis, elle est manipulée par la CIA",
-        "https://www.radiofrance.fr/franceinter/podcasts/espions-une-histoire-vraie/vadim-krasikov-le-tueur-prefere-de-poutine-4287237|Qui aurait pu croire que cette vieille dame anglaise avait trahi son pays ? Melita Norwood est pour ainsi dire l'espionne parfaite, celle dont rêvent tous les services de renseignement. Efficace, courageuse, discrète, celle qui, pendant des décennies, a agi dans l'ombre sans être jamais découverte.",
-        "https://www.radiofrance.fr/franceculture/podcasts/les-grandes-traversees/naissance-d-un-prodige-6979092|À 12 ans, détenu avec sa mère au camp d'internement de Rieucros en Lozère, Alexandre Grothendieck découvre la beauté du cercle et des polyèdres. Pour échapper au tumulte de la guerre, l'adolescent juif apatride se réfugie dans l'abstraction.",
-        "https://www.radiofrance.fr/franceculture/podcasts/les-grandes-traversees/les-mots-qui-tuent-6008423|Comment vit-on quand un mot peut être fatal ? Qu'est-ce que la langue raconte de la vie sous Staline ?",
-        "https://www.radiofrance.fr/franceculture/podcasts/les-grandes-traversees/l-etoile-de-papier-1181737|Kafka sur la plage, jouant au milieu des enfants. Son carnet d'hébreu, son rêve de Palestine, sa dernière année à Berlin avec Dora Diamant et l'éternelle question : de quoi ai-je hérité et que vais-je à mon tour laisser ?",
+        "https://www.radiofrance.fr/franceinter/podcasts/espions-une-histoire-vraie/vadim-krasikov-le-tueur-prefere-de-poutine-4287237|Qui aurait pu croire que cette vieille dame anglaise avait trahi son pays ? Melita Norwood est pour ainsi dire l’espionne parfaite, celle dont rêvent tous les services de renseignement. Efficace, courageuse, discrète, celle qui, pendant des décennies, a agi dans l’ombre sans être jamais découverte.",
+        "https://www.radiofrance.fr/franceculture/podcasts/les-grandes-traversees/naissance-d-un-prodige-6979092|À 12 ans, détenu avec sa mère au camp d'internement de Rieucros en Lozère, Alexandre Grothendieck découvre la beauté du cercle et des polyèdres. Pour échapper au tumulte de la guerre, l'adolescent juif apatride se réfugie dans l’abstraction.",
+        "https://www.radiofrance.fr/franceculture/podcasts/les-grandes-traversees/les-mots-qui-tuent-6008423|Comment vit-on quand un mot peut être fatal ? Qu'est-ce que la langue raconte de la vie sous Staline ?",
+        "https://www.radiofrance.fr/franceculture/podcasts/les-grandes-traversees/l-etoile-de-papier-1181737|Kafka sur la plage, jouant au milieu des enfants. Son carnet d’hébreu, son rêve de Palestine, sa dernière année à Berlin avec Dora Diamant et l’éternelle question : de quoi ai-je hérité et que vais-je à mon tour laisser ?",
     }, delimiter = '|')
     void testSubtitle(final String url,
                       final String expectedSubtitle) {
